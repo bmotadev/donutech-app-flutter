1b3cdd252c7aae0e2cf975d8808c591c3f32eb6b
Bruno Mota <bmotadev@gmail.com>
2023-03-26 11:12:31 -0300

    :sparkles: feat: tabs para os icones

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:donutech_appui/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,51 +9,96 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // my tabs
+  List<Widget> myTabs = [
+    // tab donut
+    MyTab(
+      iconPath: 'assets/icons/donut.png',
+    ),
+    // tab burger
+    MyTab(
+      iconPath: 'assets/icons/donut.png',
+    ),
+    // tab smoothie
+    MyTab(
+      iconPath: 'assets/icons/donut.png',
+    ),
+    // tab panqueca
+    MyTab(
+      iconPath: 'assets/icons/donut.png',
+    ),
+    // tab pizza
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 24.0),
-          child: IconButton(
-            icon: Icon(
-              Icons.menu,
-              color: Colors.grey[800],
-              size: 36,
-            ),
-            onPressed: () {
-              // abre o menu
-            },
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 24.0),
             child: IconButton(
               icon: Icon(
-                Icons.person,
+                Icons.menu,
                 color: Colors.grey[800],
                 size: 36,
               ),
               onPressed: () {
-                // botão da conta
+                // abre o menu
               },
             ),
           ),
-        ],
-      ),
-      body: Column(
-        children: [
-          // i want to eat
-          Text(
-            'I want to eat',
-          ),
-          // tab bar
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: IconButton(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.grey[800],
+                  size: 36,
+                ),
+                onPressed: () {
+                  // botão da conta
+                },
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            // i want to eat
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 36.0, vertical: 18),
+              child: Row(
+                children: const [
+                  Text(
+                    'I want to eat',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  Text(
+                    'EAT',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
-          // tab bar view
-        ],
+            const SizedBox(
+              height: 24,
+            ),
+
+            // aba da barra
+            TabBar(tabs: myTabs)
+            // aba da barra view
+          ],
+        ),
       ),
     );
   }
